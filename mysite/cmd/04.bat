499be5e1b2c291be21aae7bfefe92ebf887d9ccd
linuxwd <5323547@gmail.com>
2016-10-05 04:50:58 +0800

    基于sqlite3的migrate资料库创建及同步完成

---
+++ mysite/cmd/04.bat
@@ -0,0 +1,2 @@
+cd c:\Users\Administrator\Desktop\Django版餐厅管理系统\Django-mysite\mysite\
+c:\Python35\python.exe c:\Users\Administrator\Desktop\Django版餐厅管理系统\Django-mysite\mysite\manage.py makemigrations restaurants
--- mysite/restaurants/models.py
+++ mysite/restaurants/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+class Restaurant(models.Model):
+    #列名name，字符型，最大长度20
+    name = models.CharField(max_length=20)
+    #列名phone_number，字符型，最大长度15
+    phone_number = models.CharField(max_length=15)
+    #列名address，字符型，最大长度50，允许该字段为空（blank）
+    address = models.CharField(max_length=50,blank=True)
+
+class Food(models.Model):
+    #列名name，字符型，最大长度20
+    name = models.CharField(max_length=20)
+    #列名price，浮点型，max_digits表示数字允许的最大位数（包括小数），decimal_places表示小数字数为2位小数
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    # 列名comment，字符型，最大长度50，blank=True表示该字段允许位空值
+    comment = models.CharField(max_length=50,blank=True)
+    #列名is_spicy，布尔型，默认值为false
+    is_spicy = models.BooleanField(default=False)
+    #列名restaurant，外键只想Restaurant表
+    restaurant = models.ForeignKey(Restaurant)
